dfd3c52e39274a3f47ff089d784fdc8511f935f3
Ruth Cisneros <senoritaruth@gmail.com>
2019-08-01 08:56:17 -0600

    making flex happen one command at a time

--- snap_8_1/snap1.php
+++ snap_8_1/snap1.php
@@ -7,6 +7,7 @@
 	</head>
 	<body>
 		<h1>Snap Challenge for August 1st, 2019</h1>
+		<h2>Classic CSS practice</h2>
 		<ul class="my-list">
 			<li class="my-item">1</li>
 			<li class="my-item">2</li>
@@ -17,4 +18,15 @@
 		</ul>
 		<p>The width of the box is: 210px.</p>
 		<p>The height of the box is: 170px.</p>
+		<br>
+		<h2>Flexbox Fight</h2>
+		<ul class="my-list2">
+			<li class="my-item2">1</li>
+			<li class="my-item2">2</li>
+			<li class="my-item2">3</li>
+			<li class="my-item2">4</li>
+			<li class="my-item2">5</li>
+			<li class="my-item2">6</li>
+		</ul>
+
 	</body>
